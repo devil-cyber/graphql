bc80b19ba456ef49b05e90e89bb1926b460fbce5
Manikant Singh <mani2474695@gmail.com>
2020-10-24 10:45:34 +0530

    server started

---
+++ dist/index.dev.js
@@ -0,0 +1,8 @@
+"use strict";
+
+var express = require('express');
+
+var app = express();
+app.listen(4000, function (err) {
+  console.log('Listining at port 4000');
+});
---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+
+
+// TODO - Listen server at specific port
+app.listen(4000, (err) => {
+    console.log('Listining at port 4000');
+})
