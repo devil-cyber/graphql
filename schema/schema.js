ca0ac5f65474e5f98c5d8eeb2a7ae6543524194a
Manikant Singh <mani2474695@gmail.com>
2020-10-24 16:44:28 +0530

    added check for null data to appear in database

--- schema/schema.js
+++ schema/schema.js
@@ -9,6 +9,7 @@ const {
     GraphQLString,
     GraphQLInt,
     GraphQLSchema,
+    GraphQLNonNull,
     GraphQLList
 } = graphql;
 
@@ -155,10 +156,10 @@ const Mutation = new GraphQLObjectType({
             type: AuthorType,
             args: {
                 name: {
-                    type: GraphQLString
+                    type: new GraphQLNonNull(GraphQLString)
                 },
                 age: {
-                    type: GraphQLInt
+                    type: new GraphQLNonNull(GraphQLInt)
                 }
             },
             resolve(parent, args) {
@@ -173,13 +174,13 @@ const Mutation = new GraphQLObjectType({
             type: BookType,
             args: {
                 name: {
-                    type: GraphQLString
+                    type: new GraphQLNonNull(GraphQLString)
                 },
                 genre: {
-                    type: GraphQLString
+                    type: new GraphQLNonNull(GraphQLString)
                 },
                 authorId: {
-                    type: GraphQLID
+                    type: new GraphQLNonNull(GraphQLID)
                 }
             },
             resolve(parent, args) {
